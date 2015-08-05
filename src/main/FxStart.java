c517633180ca93b126730a6565ffc3ba4777e746
Andrew <amverhagen93@gmail.com>
2015-08-05 15:44:50 -0400

    Create initial stage for fx version

--- src/main/FxStart.java
+++ src/main/FxStart.java
@@ -1,9 +1,22 @@
 package main;
 
-public class FxStart {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FxStart extends Application {
 
 	public static void main(String[] args) {
-		System.out.println("Fx go");
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setHeight(600);
+		stage.setWidth(600);
+		stage.centerOnScreen();
+		stage.setResizable(false);
+		stage.setTitle("Sort");
+		stage.show();
 	}
 
 }
