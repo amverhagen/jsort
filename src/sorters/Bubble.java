d19d3c40ab9e69fdd197ba6f4ca80b2aab35da28
Andrew <amverhagen93@gmail.com>
2015-08-07 23:21:36 -0400

    Implement bubble sort

--- src/sorters/Bubble.java
+++ src/sorters/Bubble.java
@@ -1,18 +1,37 @@
 package sorters;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Bubble {
 
 	public static int[] getSortedList(int[] unsorted) {
-		for (int i = unsorted.length; i > 0; i--) {
-			System.out.println(i);
+		int[] sorted = unsorted;
+		for (int i = sorted.length; i > 0; i--) {
+			for (int j = 0; j < (i - 1); j++) {
+				if (sorted[j] > sorted[j + 1]) {
+					int temp = sorted[j];
+					sorted[j] = sorted[j + 1];
+					sorted[j + 1] = temp;
+				}
+			}
 		}
-		return unsorted;
+		return sorted;
 	}
 
 	public static ArrayList<String> getSortedStepList(int[] unsorted) {
 		ArrayList<String> sorted = new ArrayList<String>();
+		for (int i = unsorted.length; i > 0; i--) {
+			for (int j = 0; j < (i - 1); j++) {
+				if (unsorted[j] > unsorted[j + 1]) {
+					int temp = unsorted[j];
+					unsorted[j] = unsorted[j + 1];
+					unsorted[j + 1] = temp;
+					sorted.add("Swapped " + temp + " and " + unsorted[j]);
+					sorted.add(Arrays.toString(unsorted));
+				}
+			}
+		}
 		return sorted;
 	}
 }
